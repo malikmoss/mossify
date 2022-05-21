40c421649d5c583f08e0863a1491976d196f8d35
malikmoss <malikmoss33@gmail.com>
2022-05-21 12:05:43 -0500

    playlist containet setup

--- myapp/components/playerLayout.tsx
+++ myapp/components/playerLayout.tsx
@@ -3,9 +3,16 @@ import { Box } from "@chakra-ui/layout";
 //component that provides layout of sider bar, music bar and houses actual page
 const PlayerLayout = ({ children }) => {
     return (
-      <Box>
-          Layout
-        {children}
+      <Box width="100vw" height="100vh">
+        <Box position="absolute" top="0" width="250px" left="0">
+            sidebar
+        </Box>
+        <Box marginLeft="250px">
+            {children}
+        </Box>
+        <Box position="absolute" left="0" bottom="0">
+            player
+        </Box>
       </Box>
     )
 }
