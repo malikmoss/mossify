f96c7d7d6744073132ae330c8f68643c1e2c1a48
malikmoss <malikmoss33@gmail.com>
2022-05-21 11:58:02 -0500

    player layout created

---
+++ myapp/components/playerLayout.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/layout";
+
+//component that provides layout of sider bar, music bar and houses actual page
+const PlayerLayout = ({ children }) => {
+    return (
+      <Box>
+          Layout
+        {children}
+      </Box>
+    )
+}
+
+export default PlayerLayout;
