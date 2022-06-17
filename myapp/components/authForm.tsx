b376845e40ab6038fb4185c666cf62dd49aa0a34
malikmoss <malik@govalo.com>
2022-06-17 17:19:32 -0500

    Auth Pages still needs testing

---
+++ myapp/components/authForm.tsx
@@ -0,0 +1,21 @@
+import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { FC, useState } from "react";
+import { MdEmail } from "react-icons/md";
+import { useSWRConfig } from "swr";
+import { auth } from "../lib/mutations";
+
+const AuthForm: FC<{ mode: string }> = ({ mode }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Box height="100vh" width="100vw" bg="black">
+      <Flex justify="center" align="center" height="100px"></Flex>
+    </Box>
+  );
+};
+
+export default AuthForm;
