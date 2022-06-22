1a5c65a7acbae3b346ac818826b6ab61ed2f40e7
malikmoss <malik@govalo.com>
2022-06-22 14:52:33 -0500

    gradient bg and image box cont.

--- myapp/components/playerLayout.tsx
+++ myapp/components/playerLayout.tsx
@@ -3,19 +3,19 @@ import Sidebar from "./sidebar";
 
 //component that provides layout of sider bar, music bar and houses actual page
 const PlayerLayout = ({ children }) => {
-    return (
-      <Box width="100vw" height="100vh">
-        <Box position="absolute" top="0" width="250px" left="0">
-            <Sidebar />
-        </Box>
-        <Box marginLeft="250px">
-            {children}
-        </Box>
-        <Box position="absolute" left="0" bottom="0">
-            player
-        </Box>
+  return (
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        <Sidebar />
       </Box>
-    )
-}
+      <Box marginLeft="250px">
+        <Box height="calc(100vh - 100px)">{children}</Box>
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        player
+      </Box>
+    </Box>
+  );
+};
 
 export default PlayerLayout;
--- myapp/pages/index.tsx
+++ myapp/pages/index.tsx
@@ -1,7 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import GradientLayout from "../components/gradientLayout";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <div>Home</div>
+export default function Home = () => {
+  return (
+    <GradientLayout color="red">
+      <div>Home page</div>
+    </GradientLayout>
+  );
 }
