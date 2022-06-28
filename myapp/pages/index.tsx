c817ce29267bba9df999d8e20390288a20f7e7ed
malikmoss <malik@govalo.com>
2022-06-28 13:55:42 -0500

    loads artists data

--- myapp/pages/index.tsx
+++ myapp/pages/index.tsx
@@ -2,13 +2,28 @@ import Head from "next/head";
 import Image from "next/image";
 import GradientLayout from "../components/gradientLayout";
 import styles from "../styles/Home.module.css";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+const Home = (artists) => {
   return (
-    <GradientLayout color="red">
+    <GradientLayout
+      roundImage
+      color="green"
+      subtitle="profile"
+      title="Malik Moss"
+      description="15 public playlists"
+    >
       <div>Home page</div>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async (req) => {
+  const artists = await prisma.artist.findMany();
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
