75c2caf1f981a6c00279171d81c9719ecda3cd36
malikmoss <malik@govalo.com>
2022-06-28 22:30:59 -0500

    adjustments to me.ts

--- myapp/pages/api/me.ts
+++ myapp/pages/api/me.ts
@@ -1,11 +1,13 @@
 import { validateRoute } from "../../lib/auth";
 import prisma from "../../lib/prisma";
-//function to get the user
 
 export default validateRoute(async (req, res, user) => {
   const playlistsCount = await prisma.playlist.count({
-    where: { userId: user.id },
+    where: {
+      userId: user.id,
+    },
   });
+
   console.log(playlistsCount);
-  res.json(...user, playlistsCount);
+  res.json({ ...user, playlistsCount });
 });
