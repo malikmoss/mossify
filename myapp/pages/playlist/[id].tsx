94bf7d56929004938dc7d563ef4b9535f79afb52
malikmoss <malik@govalo.com>
2022-06-28 22:47:16 -0500

    songs table ui

--- myapp/pages/playlist/[id].tsx
+++ myapp/pages/playlist/[id].tsx
@@ -1,6 +1,7 @@
 import prisma from "../../lib/prisma";
 import { validateToken } from "../../lib/auth";
 import GradientLayout from "../../components/gradientLayout";
+import SongTable from "../../components/songsTable";
 
 const getBGColor = (id) => {
   const colors = [
@@ -27,7 +28,9 @@ const Playlist = ({ playlist }) => {
       subtitle="playlist"
       description={`${playlist.songs.length} songs`}
       image={`https://picsum.photos/400?random=${playlist.id}`}
-    ></GradientLayout>
+    >
+      <SongTable songs={playlist.songs} />
+    </GradientLayout>
   );
 };
 
