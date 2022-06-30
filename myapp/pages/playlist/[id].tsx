68620fdb9823ce8081c917302225242e10ec0f0b
malikmoss <malik@govalo.com>
2022-06-30 14:58:11 -0500

    auth error handling

--- myapp/pages/playlist/[id].tsx
+++ myapp/pages/playlist/[id].tsx
@@ -2,6 +2,7 @@ import prisma from "../../lib/prisma";
 import { validateToken } from "../../lib/auth";
 import GradientLayout from "../../components/gradientLayout";
 import SongTable from "../../components/songsTable";
+import { formatDate, formatTime } from "../../lib/formatters";
 
 const getBGColor = (id) => {
   const colors = [
@@ -35,6 +36,18 @@ const Playlist = ({ playlist }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
+  let user;
+
+  try {
+    user = validateToken(req.cookies.MOSSIFY_ACCESS_TOKEN);
+  } catch (e) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/signin",
+      },
+    };
+  }
   const { id } = validateToken(req.cookies.MOSSIFY_ACCESS_TOKEN);
   const [playlist] = await prisma.playlist.findMany({
     where: {
