d48df03a1674d1d263edca437010839612662153
malikmoss <malik@govalo.com>
2022-06-30 23:05:43 -0500

    player container ui

---
+++ myapp/components/playerBar.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import Sidebar from "./sidebar";
+
+const PlayerBar = () => {
+  return (
+    <Box height="100px" width="100vw" bg="gray.900" padding="10px">
+      <Flex>
+        <Box padding="20px" color="white" width="30%">
+          <Text fontSize="large">Song Name</Text>
+          <Text fontSize="small">Artist Name</Text>
+        </Box>
+        <Box width="40%"></Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default PlayerBar;
--- myapp/components/playerLayout.tsx
+++ myapp/components/playerLayout.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/layout";
 import Sidebar from "./sidebar";
+import PlayerBar from "./playerBar";
 
 //component that provides layout of sider bar, music bar and houses actual page
 const PlayerLayout = ({ children }) => {
@@ -12,7 +13,7 @@ const PlayerLayout = ({ children }) => {
         <Box height="calc(100vh - 100px)">{children}</Box>
       </Box>
       <Box position="absolute" left="0" bottom="0">
-        player
+        <PlayerBar />
       </Box>
     </Box>
   );
