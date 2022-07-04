8176db32b6e852d1601d237934df453b311f3d5d
malikmoss <malik@govalo.com>
2022-07-03 22:14:40 -0500

    adjustments to some player component files

--- myapp/components/playerBar.tsx
+++ myapp/components/playerBar.tsx
@@ -1,17 +1,18 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
-import Player from "./player";
 import { useStoreState } from "easy-peasy";
+import Player from "./player";
 
 const PlayerBar = () => {
   const songs = useStoreState((state: any) => state.activeSongs);
   const activeSong = useStoreState((state: any) => state.activeSong);
+
   return (
     <Box height="100px" width="100vw" bg="gray.900" padding="10px">
-      <Flex>
+      <Flex align="center">
         {activeSong ? (
           <Box padding="20px" color="white" width="30%">
             <Text fontSize="large">{activeSong.name}</Text>
-            <Text fontSize="small">{activeSong.artist.name}</Text>
+            <Text fontSize="sm">{activeSong.artist.name}</Text>
           </Box>
         ) : null}
         <Box width="40%">
